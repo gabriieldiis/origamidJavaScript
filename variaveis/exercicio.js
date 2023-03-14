a445b62b63ba8689f771cf28d5b7dd960fca392d
Gabriel Dias <gabrieldias1968@gmail.com>
2023-03-14 00:05:48 -0300

    commit variaveis

---
+++ variaveis/exercicio.js
@@ -0,0 +1,17 @@
+var nome = 'Gabriel';
+
+var idade = 22;
+
+var comidaFavorita;
+
+comidaFavorita = 'Macarrão com queijo';
+
+var nomeCachorro;
+var nomeMae;
+var nomeNamorada;
+var nomePai;
+var nomeProfessor;
+
+console.log (nome);
+console.log (idade);
+console.log (comidaFavorita);
---
+++ variaveis/script.js
@@ -0,0 +1,20 @@
+console.log('Olá mundo');
+
+var nome = 'Gabriel';
+var idade = 22 ; 
+console.log(nome, idade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco
+
+console.log(totalPreco);
+
+var sobrenome = 'Dias',
+    cidade = 'SP';
+
+console.log(sobrenome, cidade);
+
+// var semDefinir;
+
+// console.log(semDefinir);
